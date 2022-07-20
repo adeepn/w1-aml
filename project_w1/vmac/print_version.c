ab0b0841c4982f26c22c351462c0df4f1231abbe
Vyacheslav Bocharov <adeep@lexina.in>
2022-07-20 11:33:28 +0300

    Add print_version

---
+++ project_w1/vmac/print_version.c
@@ -0,0 +1,6 @@
+#include "wifi_hal_com.h"
+
+void print_driver_version(void) {
+    printk("driver compile date: 2022-07-20 10:32:03,driver hash: 61503b27ca81beb8afaec871e37804e06d2bbdfc\n");
+    printk("fw compile date: 2021-10-29 15:22:44,fw hash: 61503b27ca81beb8afaec871e37804e06d2bbdfc,fw size: 1071764\n");
+}
